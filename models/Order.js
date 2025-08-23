5aac8b78e5700e74fc3b86f3936150d6bb023805
Ramanabhai <ramanamathi2003@gmail.com>
2025-08-24 02:56:44 +0530

    updated

--- models/Order.js
+++ models/Order.js
@@ -17,6 +17,7 @@ const orderSchema = new mongoose.Schema({
   },
   paymentMethod: { type: String, required: true },
   totalPrice: { type: Number, required: true },
+  packed: { type: Boolean, default: false },
 }, { timestamps: true });
 
 
